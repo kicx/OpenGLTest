964c5cdfb2f0b97475ffa7db2181aaee9eebf687
Lee Cadena <bboykicx@gmail.com>
2017-08-28 09:49:42 -0400

    red screen added custom renderer

---
+++ app/src/main/java/com/dreams/foggy/opengltest/MainActivity.java
@@ -0,0 +1,61 @@
+package com.dreams.foggy.opengltest;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private GLSurfaceView glSurfaceView;
+    private boolean rendererSet = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        glSurfaceView = new GLSurfaceView(this);
+
+
+        final ActivityManager activityManager = (ActivityManager)getSystemService(Context.ACTIVITY_SERVICE);
+        final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+
+        final boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000;
+
+        if(supportsEs2){
+            glSurfaceView.setEGLContextClientVersion(2);
+            glSurfaceView.setRenderer(new Renderer());
+            rendererSet = true;
+        }else{
+            Toast.makeText(this, "this device does not support OpenGL ES 2.0",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        setContentView(glSurfaceView);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(rendererSet){
+            glSurfaceView.onPause();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(rendererSet){
+            glSurfaceView.onResume();
+        }
+    }
+}
